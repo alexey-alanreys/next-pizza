0bca4624a4900f9d62affa994fb0017b0a5fa893
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-10-27 14:20:40 +1000

    Add ProductsGroupList shared component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -5,6 +5,7 @@ export { FilterCheckbox } from './filter-checkbox';
 export { Filters } from './filters';
 export { Header } from './header';
 export { ProductCard } from './product-card';
+export { ProductsGroupList } from './products-group-list';
 export { SortPopup } from './sort-popup';
 export { Title } from './title';
 export { TopBar } from '../shared/top-bar';
---
+++ src/components/shared/products-group-list.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+import { ProductCard } from './product-card';
+import { Title } from './title';
+
+interface Props {
+	title: string;
+	products: any[];
+	categoryId: number;
+	className?: string;
+	listClassName?: string;
+}
+
+export const ProductsGroupList: React.FC<Props> = ({
+	title,
+	products,
+	categoryId,
+	listClassName,
+	className,
+}) => {
+	return (
+		<div className={className} id={title}>
+			<Title text={title} size='lg' className='font-extrabold mb-5' />
+			<div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+				{products
+					.filter((product) => product.items.length > 0)
+					.map((product, index) => (
+						<ProductCard
+							key={product.id}
+							id={product.id}
+							name={product.name}
+							imageUrl={product.imageUrl}
+							price={product.items[0].price}
+						/>
+					))}
+			</div>
+		</div>
+	);
+};
