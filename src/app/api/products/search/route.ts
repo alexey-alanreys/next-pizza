79f154a9b964f099ea6d020cf75cbdf4a4e9af07
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-10-28 12:03:39 +1000

    Add product search and details API routes

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+
+import { GetSearchParams } from '@/lib/find-pizzas';
+import { getSearchParams } from '@/lib/get-search-params';
+import { prisma } from '@/lib/prisma';
+
+export async function GET(req: Request) {
+	const params = getSearchParams<GetSearchParams>(req.url);
+
+	const products = await prisma.product.findMany({
+		where: {
+			name: {
+				contains: params.query,
+				mode: 'insensitive',
+			},
+		},
+		take: 5,
+	});
+
+	console.log(products);
+
+	return NextResponse.json(products);
+}
